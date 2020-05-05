d1a27e2a25fc99cbe42bbdc992c9da3292ebadc7
nnn1590 <nnn1590@nnn1590.org>
2020-05-06 02:11:02 +0900

    Update makemy-dist.sh

--- makemy-dist.sh
+++ makemy-dist.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
-echo ==========
-echo makemydist
-echo ==========
-echo 設定ファイルのコピーを開始します。root権限を使用します。
-sudo cp -r $HOME/.local/share/applications/ /etc/skel/.local/
-sudo cp -r $HOME/.config/autostart/ /etc/skel/.config/
-sudo cp -r $HOME/.config/dconf/ /etc/skel/.config/
-sudo cp -r $HOME/.config/gtk-2.0/ /etc/skel/.config/
-sudo cp -r $HOME/.config/gtk-3.0/ /etc/skel/.config/
-sudo cp -r $HOME/.config/Thunar/ /etc/skel/.config/
-sudo cp -r $HOME/.config/xfce4/ /etc/skel/.config/
-echo システムをアップデートします
-sudo apt-get update && apt-get upgrade -y
-echo 不要な依存関係を削除します
-sudo apt autoremove
-sudo PinguyBuilder clean
-echo 準備は完了しました。
-sudo PinguyBuilder dist
-sudo PinguyBuilder dist iso mydist.iso
+#!/bin/bash -e +H
+echo '==========================='
+echo '        makemydist         '
+echo 'Root privilege is required.'
+echo '==========================='
+echo 'Copying settings files...'
+sudo -E -- cp -r "${HOME}/.local/share/applications/" "/etc/skel/.local/"
+sudo -E -- cp -r "${HOME}/.config/autostart/"         "/etc/skel/.config/"
+sudo -E -- cp -r "${HOME}/.config/dconf/"             "/etc/skel/.config/"
+sudo -E -- cp -r "${HOME}/.config/gtk-2.0/"           "/etc/skel/.config/"
+sudo -E -- cp -r "${HOME}/.config/gtk-3.0/"           "/etc/skel/.config/"
+sudo -E -- cp -r "${HOME}/.config/Thunar/"            "/etc/skel/.config/"
+sudo -E -- cp -r "${HOME}/.config/xfce4/"             "/etc/skel/.config/"
+echo 'Upgrading system...'
+sudo -E -- apt update && sudo -E -- apt upgrade -y
+echo 'Remove unused dependencies...'
+sudo -E -- apt autoremove
+sudo -E -- PinguyBuilder clean
+echo 'Ready to build.'
+sudo -E -- PinguyBuilder dist
+sudo -E -- PinguyBuilder dist iso mydist.iso
+echo 'Done!'
